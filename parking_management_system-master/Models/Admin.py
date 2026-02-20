221f5a01b3ec2f264e3f8bb68fb1ae3105957bb8
yadusaheb5-oss <yadusaheb5@gmail.com>
2026-02-20 19:38:37 +0530

    Update Admin.py

--- parking_management_system-master/Models/Admin.py
+++ parking_management_system-master/Models/Admin.py
@@ -1,7 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
+from app import db
+ 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
